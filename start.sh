8baa2ba82f6a4d857805b5a47a1acbf334e6949e
GitHubNull <641570479@qq.com>
2017-01-06 14:20:41 +0800

    初步完成任务了，未测试。

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt-get install build-essential
+sudo apt-get install libgtk2.0-dev
+gcc -shared -o libsublime-imfix.so sublime-imfix.c `pkg-config --libs --cflags gtk+-2.0` -fPIC
+LD_PRELOAD=./libsublime-imfix.so subl
+sudo cp -y sublime_text.desktop /usr/share/applications/sublime_text.desktop
+sudo cp -y libsublime-imfix.so /opt/sublime_text/
+sudo cp -y subl  /usr/bin/subl

dda351d94010c4c0513e565cd2ba6968c5cf6c76
GitHubNull <641570479@qq.com>
2017-01-10 10:33:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ sudo apt-get install build-essential
 sudo apt-get install libgtk2.0-dev
 gcc -shared -o libsublime-imfix.so sublime-imfix.c `pkg-config --libs --cflags gtk+-2.0` -fPIC
 LD_PRELOAD=./libsublime-imfix.so subl
-sudo cp -y sublime_text.desktop /usr/share/applications/sublime_text.desktop
-sudo cp -y libsublime-imfix.so /opt/sublime_text/
-sudo cp -y subl  /usr/bin/subl
+sudo mv sublime_text.desktop /usr/share/applications/sublime_text.desktop
+sudo mv libsublime-imfix.so /opt/sublime_text/
+sudo mv subl  /usr/bin/subl
 cd ../
 rm -rf fix_sublime_zh
